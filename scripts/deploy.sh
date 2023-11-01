04481dd2b2e0d00b1ef81d310b80ea93ea525989
beombu <beombu13@gmail.com>
2023-11-01 13:38:58 +0900

    [REV-54] cicd: BeforeIntstall 스크립트 추가

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -23,9 +23,4 @@ else
 fi
 
 echo "> 배포 - $JAR_PATH"
-chmod +x $JAR_PATH
-
-source /home/ubuntu/app/scripts/properties.sh
-sleep 5
-
 nohup java -jar $JAR_PATH > /dev/null 2> /dev/null < /dev/null &
