9bb6d86b2d0939cd800ec9af8871af2edc45f70c
juwoong kim <wwong7247@gmail.com>
2023-11-02 01:51:30 +0900

    [REV-85] refactor: dto 엔티티 변환 작업을 막는 유효성검사 해제

--- src/main/java/com/devcourse/ReviewRanger/surveyresult/domain/SurveyResult.java
+++ src/main/java/com/devcourse/ReviewRanger/surveyresult/domain/SurveyResult.java
@@ -16,27 +16,29 @@ import lombok.Getter;
 public class SurveyResult extends BaseEntity {
 
 	@Column(name = "survey_id", nullable = false)
-	@NotBlank(message = "설문 Id는 빈값 일 수 없습니다.")
 	private Long surveyId;
 
 	@Column(name = "responser_id", nullable = false)
-	@NotBlank(message = "응답자 Id는 빈값 일 수 없습니다.")
 	private Long responserId;
 
 	@Column(name = "deadline_status", nullable = false)
 	@Enumerated(EnumType.STRING)
-	private DeadlineStatus deadlineStatus;
+	private DeadlineStatus deadlineStatus = DeadlineStatus.PROCEEDING;
+	;
 
 	@Column(name = "question_answered_status", nullable = false)
-	private Boolean questionAnsweredStatus;
+	private Boolean questionAnsweredStatus = false;
 
 	protected SurveyResult() {
 	}
 
-	public SurveyResult(Long surveyId, Long responserId) {
-		this.surveyId = surveyId;
+	public SurveyResult(Long responserId) {
 		this.responserId = responserId;
 		this.deadlineStatus = DeadlineStatus.PROCEEDING;
 		this.questionAnsweredStatus = false;
+	}
+
+	public void assignSurveyId(Long surveyId) {
+		this.surveyId = surveyId;
 	}
 }
