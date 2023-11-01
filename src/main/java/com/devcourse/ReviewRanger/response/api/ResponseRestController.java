f6a7108e4e65bf51b8e02eccc6d38f43534d502b
beombu <beombu13@gmail.com>
2023-11-02 01:45:15 +0900

    [REV-119] feat: 응답 컨트롤러 구현

---
+++ src/main/java/com/devcourse/ReviewRanger/response/api/ResponseRestController.java
@@ -0,0 +1,28 @@
+package com.devcourse.ReviewRanger.response.api;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.devcourse.ReviewRanger.response.application.ResponseService;
+import com.devcourse.ReviewRanger.response.dto.request.CreateResponse;
+
+import jakarta.validation.Valid;
+
+@RestController
+public class ResponseRestController {
+
+	private final ResponseService responseService;
+
+	public ResponseRestController(ResponseService responseService) {
+		this.responseService = responseService;
+	}
+
+	@PostMapping(value = "/invited-surveys")
+	public ResponseEntity<Boolean> create(@RequestBody @Valid CreateResponse request) {
+		return ResponseEntity.status(HttpStatus.CREATED)
+			.body(responseService.create(request));
+	}
+}
