7d499bf4d8995101619335ac5d19d7802f2f6c1b
beombu <beombu13@gmail.com>
2023-11-02 02:22:25 +0900

    [REV-137] feat: swagger 초기 세팅 구현

---
+++ src/main/java/com/devcourse/ReviewRanger/common/config/SwaggerConfig.java
@@ -0,0 +1,46 @@
+package com.devcourse.ReviewRanger.common.config;
+
+import org.springdoc.core.models.GroupedOpenApi;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.info.Info;
+
+@OpenAPIDefinition(
+	info = @Info(title = "리뷰레인저 서비스",
+		description = "리뷰레인저 api 명세서",
+		version = "v1"))
+@Configuration
+public class SwaggerConfig {
+
+	@Bean
+	public GroupedOpenApi userOpenApi() {
+		String[] paths = {"/members/**", "/sign-up"};
+
+		return GroupedOpenApi.builder()
+			.group("유저 API v1")
+			.pathsToMatch(paths)
+			.build();
+	}
+
+	@Bean
+	public GroupedOpenApi surveyOpenApi() {
+		String[] paths = {"/surveys/**"};
+
+		return GroupedOpenApi.builder()
+			.group("설문 API v1")
+			.pathsToMatch(paths)
+			.build();
+	}
+
+	@Bean
+	public GroupedOpenApi responseOpenApi() {
+		String[] paths = {"/responses/**", "/invited-surveys"};
+
+		return GroupedOpenApi.builder()
+			.group("응답 API v1")
+			.pathsToMatch(paths)
+			.build();
+	}
+}
