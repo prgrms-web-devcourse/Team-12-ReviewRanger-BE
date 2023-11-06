682cd71a2bd66afd3b642da7c96d85741aa0869b
sujjangOvO <ssu7038@yu.ac.kr>
2023-11-07 02:40:19 +0900

    modify: 최종 리뷰 결과 및 응답 Entity 네이밍 수정

--- src/main/java/com/devcourse/ReviewRanger/finalReviewResult/domain/FinalReviewResult.java
+++ src/main/java/com/devcourse/ReviewRanger/finalReviewResult/domain/FinalReviewResult.java
@@ -8,6 +8,9 @@ import javax.validation.constraints.Size;
 
 import com.devcourse.ReviewRanger.BaseEntity;
 
+import lombok.Getter;
+
+@Getter
 @Entity
 @Table(name = "final_review_results")
 public class FinalReviewResult extends BaseEntity {
@@ -16,12 +19,12 @@ public class FinalReviewResult extends BaseEntity {
 	@NotBlank(message = "리뷰 대상 id는 빈값 일 수 없습니다.")
 	private Long userId;
 
-	@Column(name = "survey_id", nullable = false)
-	@NotBlank(message = "설문 id는 빈값 일 수 없습니다.")
+	@Column(name = "review_id", nullable = false)
+	@NotBlank(message = "리뷰 id는 빈값 일 수 없습니다.")
 	private Long reviewId;
 
 	@Column(nullable = false, length = 50)
-	@NotBlank(message = "설문제목은 빈값 일 수 없습니다.")
+	@NotBlank(message = "리뷰 제목은 빈값 일 수 없습니다.")
 	@Size(max = 50, message = "50자 이하로 입력하세요.")
 	private String title;
 }
