912d4e79b9629915d803781b3e3dd9ae6394253c
sujjangOvO <ssu7038@yu.ac.kr>
2023-11-07 02:45:12 +0900

    feat: 유저별 리뷰 결과 전체 조회 기능 추가

---
+++ src/main/java/com/devcourse/ReviewRanger/finalReviewResult/application/FinalReviewResultService.java
@@ -0,0 +1,26 @@
+package com.devcourse.ReviewRanger.finalReviewResult.application;
+
+import java.util.List;
+
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import com.devcourse.ReviewRanger.finalReviewResult.dto.FinalReviewResultListResponse;
+import com.devcourse.ReviewRanger.finalReviewResult.repository.FinalReviewResultRepository;
+
+@Service
+@Transactional(readOnly = true)
+public class FinalReviewResultService {
+
+	private final FinalReviewResultRepository finalReviewResultRepository;
+
+	public FinalReviewResultService(FinalReviewResultRepository finalReviewResultRepository) {
+		this.finalReviewResultRepository = finalReviewResultRepository;
+	}
+
+	public List<FinalReviewResultListResponse> getAllFinalReviewResults(Long userId) {
+		return finalReviewResultRepository.findAllByUserId(userId)
+			.stream()
+			.map(FinalReviewResultListResponse::from).toList();
+	}
+}
