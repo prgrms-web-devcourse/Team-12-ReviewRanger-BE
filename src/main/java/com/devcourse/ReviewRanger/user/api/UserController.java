4f56b7236688c21f5e96c41d6a69158c3d0abeec
sujjangOvO <ssu7038@yu.ac.kr>
2023-11-08 17:13:50 +0900

    refactor: 이름 변경, 비밀번호 변경 api 분리

--- src/main/java/com/devcourse/ReviewRanger/user/api/UserController.java
+++ src/main/java/com/devcourse/ReviewRanger/user/api/UserController.java
@@ -14,13 +14,13 @@ import org.springframework.web.bind.annotation.RestController;
 
 import com.devcourse.ReviewRanger.common.response.RangerResponse;
 import com.devcourse.ReviewRanger.user.application.UserService;
-import com.devcourse.ReviewRanger.user.domain.User;
 import com.devcourse.ReviewRanger.user.domain.UserPrincipal;
 import com.devcourse.ReviewRanger.user.dto.GetUserResponse;
 import com.devcourse.ReviewRanger.user.dto.JoinRequest;
 import com.devcourse.ReviewRanger.user.dto.LoginRequest;
 import com.devcourse.ReviewRanger.user.dto.LoginResponse;
-import com.devcourse.ReviewRanger.user.dto.UpdateRequest;
+import com.devcourse.ReviewRanger.user.dto.UpdateNameRequest;
+import com.devcourse.ReviewRanger.user.dto.UpdatePasswordRequest;
 import com.devcourse.ReviewRanger.user.dto.ValidateEmailRequest;
 import com.devcourse.ReviewRanger.user.dto.ValidateNameRequest;
 
@@ -41,7 +41,8 @@ public class UserController {
 	@Tag(name = "user")
 	@Operation(summary = "회원가입", description = "사용자가 회원가입을 요청하는 API", responses = {
 		@ApiResponse(responseCode = "200", description = "회원가입 성공"),
-		@ApiResponse(responseCode = "422", description = "잘못된 로그인 정보를 입력한 경우"),
+		@ApiResponse(responseCode = "409", description = "이미 가입된 아이디를 입력한 경우"),
+		@ApiResponse(responseCode = "409", description = "이미 가입된 이메일을 입력한 경우")
 	})
 	@PostMapping("/sign-up")
 	public RangerResponse<Void> join(@RequestBody @Valid JoinRequest request) {
@@ -51,6 +52,10 @@ public class UserController {
 	}
 
 	@Tag(name = "user")
+	@Operation(summary = "로그인", description = "사용자가 로그인을 요청하는 API", responses = {
+		@ApiResponse(responseCode = "200", description = "로그인 성공"),
+		@ApiResponse(responseCode = "422", description = "잘못된 로그인 정보를 입력한 경우"),
+	})
 	@PostMapping("/login")
 	public RangerResponse<LoginResponse> login(@RequestBody @Valid LoginRequest request) {
 		LoginResponse loginResponse = userService.login(request);
@@ -59,6 +64,10 @@ public class UserController {
 	}
 
 	@Tag(name = "user")
+	@Operation(summary = "로그아웃", description = "사용자가 로그아웃을 요청하는 API", responses = {
+		@ApiResponse(responseCode = "200", description = "로그아웃 성공"),
+		@ApiResponse(responseCode = "401", description = "토큰을 넣지 않은 경우"),
+	})
 	@PostMapping("/members/logout")
 	public RangerResponse<Void> logout(
 		@RequestHeader("Authorization") String accessToken
@@ -68,6 +77,10 @@ public class UserController {
 	}
 
 	@Tag(name = "user")
+	@Operation(summary = "이름 중복검사", description = "이름 중복 검사 API", responses = {
+		@ApiResponse(responseCode = "200", description = "이름이 중복되지 않음"),
+		@ApiResponse(responseCode = "409", description = "이미 가입된 이름을 입력한 경우")
+	})
 	@PostMapping("/members/check-name")
 	public RangerResponse<Void> checkname(@RequestBody @Valid ValidateNameRequest request) {
 		boolean notExistName = userService.isNotExistName(request.name());
@@ -76,6 +89,10 @@ public class UserController {
 	}
 
 	@Tag(name = "user")
+	@Operation(summary = "이메일 중복검사", description = "이메일 중복 검사 API", responses = {
+		@ApiResponse(responseCode = "200", description = "이메일이 중복되지 않음"),
+		@ApiResponse(responseCode = "409", description = "이미 가입된 이메일을 입력한 경우")
+	})
 	@PostMapping("/members/check-email")
 	public RangerResponse<Void> checkEmail(@RequestBody @Valid ValidateEmailRequest request) {
 		boolean notExistEmail = userService.isNotExistEmail(request.email());
@@ -84,16 +101,38 @@ public class UserController {
 	}
 
 	@Tag(name = "user")
-	@PatchMapping("/members/profile")
-	public RangerResponse<Void> update(
+	@Operation(summary = "사용자 이름 변경", description = "사용자가 이름을 변경하는 API", responses = {
+		@ApiResponse(responseCode = "200", description = "이름 변경 성공"),
+		@ApiResponse(responseCode = "401", description = "토큰을 넣지 않은 경우"),
+		@ApiResponse(responseCode = "409", description = "이미 가입된 아이디를 입력한 경우")
+	})
+	@PatchMapping("/members/profile-name")
+	public RangerResponse<Void> updateName(
 		@AuthenticationPrincipal UserPrincipal user,
-		@RequestBody @Valid UpdateRequest updateRequest
+		@RequestBody @Valid UpdateNameRequest updateNameRequest
 	) {
-		userService.updateInfo(user.getId(), updateRequest);
+		userService.updateName(user.getId(), updateNameRequest.name());
 		return RangerResponse.noData();
 	}
 
 	@Tag(name = "user")
+	@Operation(summary = "사용자 비밀번호 변경", description = "사용자가 비밀번호를 변경하는 API", responses = {
+		@ApiResponse(responseCode = "200", description = "비밀번호 변경 성공"),
+		@ApiResponse(responseCode = "401", description = "토큰을 넣지 않은 경우"),
+	})
+	@PatchMapping("/members/profile-password")
+	public RangerResponse<Void> updatePassword(
+		@AuthenticationPrincipal UserPrincipal user,
+		@RequestBody @Valid UpdatePasswordRequest updatePasswordRequest
+	) {
+		userService.updatePassword(user.getId(), updatePasswordRequest.password());
+		return RangerResponse.noData();
+	}
+
+	@Tag(name = "user")
+	@Operation(summary = "사용자 전체 조회", description = "모든 사용자를 조회하는 API", responses = {
+		@ApiResponse(responseCode = "200", description = "모든 사용자 조회 성공"),
+	})
 	@GetMapping("/members")
 	public RangerResponse<List<GetUserResponse>> getAllUsers() {
 		List<GetUserResponse> allUsers = userService.getAllUsers();
