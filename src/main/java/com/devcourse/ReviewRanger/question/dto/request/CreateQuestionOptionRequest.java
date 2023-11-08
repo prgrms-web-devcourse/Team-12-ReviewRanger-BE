077774f34c0a7a8559574ac8e221df472ab0c9a4
juwoong kim <wwong7247@gmail.com>
2023-11-08 18:31:06 +0900

    [REV-166] modify: 질문옵션의 optionContext를 optionName으로 변경

--- src/main/java/com/devcourse/ReviewRanger/question/dto/request/CreateQuestionOptionRequest.java
+++ src/main/java/com/devcourse/ReviewRanger/question/dto/request/CreateQuestionOptionRequest.java
@@ -3,10 +3,10 @@ package com.devcourse.ReviewRanger.question.dto.request;
 import com.devcourse.ReviewRanger.question.domain.QuestionOption;
 
 public record CreateQuestionOptionRequest(
-	String optionContext
+	String optionName
 ) {
 
 	public QuestionOption toEntity() {
-		return new QuestionOption(this.optionContext);
+		return new QuestionOption(this.optionName);
 	}
 }
