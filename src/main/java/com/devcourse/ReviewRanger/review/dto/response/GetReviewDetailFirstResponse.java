bf3768ea8282143458b075e497bca4ac09f45120
juwoong kim <wwong7247@gmail.com>
2023-11-10 10:37:11 +0900

    [REV-170] feat: 첫 리뷰 조회시 리뷰, 질문, 리뷰 대상자 정보를 제공하는 응답객체 추가

--- src/main/java/com/devcourse/ReviewRanger/review/dto/response/GetReviewDetailFirstResponse.java
+++ src/main/java/com/devcourse/ReviewRanger/review/dto/response/GetReviewDetailFirstResponse.java
@@ -3,14 +3,14 @@ package com.devcourse.ReviewRanger.review.dto.response;
 import java.util.List;
 
 import com.devcourse.ReviewRanger.participation.domain.DeadlineStatus;
-
 import com.devcourse.ReviewRanger.question.dto.response.GetQuestionResponse;
 import com.devcourse.ReviewRanger.review.domain.Review;
+import com.devcourse.ReviewRanger.user.dto.GetUserResponse;
 
 import io.swagger.v3.oas.annotations.media.Schema;
 
-@Schema(description = "리뷰 상세 조회 응답 DTO")
-public record GetReviewDetailResponse(
+@Schema(description = "리뷰 상세 첫 조회 응답 DTO")
+public record GetReviewDetailFirstResponse(
 	@Schema(description = "리뷰 고유 id")
 	Long id,
 
@@ -24,15 +24,23 @@ public record GetReviewDetailResponse(
 	DeadlineStatus status,
 
 	@Schema(description = "리뷰에 포함된 질문 목록")
-	List<GetQuestionResponse> questions
+	List<GetQuestionResponse> questions,
+
+	@Schema(description = "응답자를 제외한 전체 리뷰 대상 목록")
+	List<GetUserResponse> receivers
 ) {
-	public GetReviewDetailResponse(Review review, List<GetQuestionResponse> questions) {
+	public GetReviewDetailFirstResponse(
+		Review review,
+		List<GetQuestionResponse> questions,
+		List<GetUserResponse> receivers
+	) {
 		this(
 			review.getId(),
 			review.getTitle(),
 			review.getDescription(),
 			review.getStatus(),
-			questions
+			questions,
+			receivers
 		);
 	}
 }
