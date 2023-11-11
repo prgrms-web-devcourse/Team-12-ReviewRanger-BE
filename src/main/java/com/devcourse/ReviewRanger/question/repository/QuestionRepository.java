7cf50a7b1e440fbd18706869f0a163e3a283780e
sujjangOvO <ssu7038@yu.ac.kr>
2023-11-11 20:42:58 +0900

    feat: 제목과 질문 타입으로 질문 찾을 수 있도록 추가

--- src/main/java/com/devcourse/ReviewRanger/question/repository/QuestionRepository.java
+++ src/main/java/com/devcourse/ReviewRanger/question/repository/QuestionRepository.java
@@ -1,13 +1,16 @@
 package com.devcourse.ReviewRanger.question.repository;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
 
 import com.devcourse.ReviewRanger.question.domain.Question;
+import com.devcourse.ReviewRanger.question.domain.QuestionType;
 
 public interface QuestionRepository extends JpaRepository<Question, Long> {
 
 	List<Question> findByReviewId(Long reviewId);
+
+	Optional<Question> findByTitleAndType(String title, QuestionType type);
 }
