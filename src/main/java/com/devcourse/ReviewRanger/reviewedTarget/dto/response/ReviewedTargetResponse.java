134b5e0bb5d02833427925e8aeb48e9a4ee07303
beombu <beombu13@gmail.com>
2023-11-15 00:37:25 +0900

    [REV-122] refactor: 리뷰 타겟, 질문 옵션 Response 생성일, 수정일 추가

--- src/main/java/com/devcourse/ReviewRanger/reviewedTarget/dto/response/ReviewedTargetResponse.java
+++ src/main/java/com/devcourse/ReviewRanger/reviewedTarget/dto/response/ReviewedTargetResponse.java
@@ -1,11 +1,14 @@
 package com.devcourse.ReviewRanger.reviewedTarget.dto.response;
 
+import java.time.LocalDateTime;
 import java.util.ArrayList;
 import java.util.List;
 
 import com.devcourse.ReviewRanger.reply.dto.response.ReplyResponse;
 import com.devcourse.ReviewRanger.reviewedTarget.domain.ReviewedTarget;
 import com.devcourse.ReviewRanger.user.dto.UserResponse;
+
+import io.swagger.v3.oas.annotations.media.Schema;
 
 public record ReviewedTargetResponse(
 	Long id,
@@ -14,14 +17,22 @@ public record ReviewedTargetResponse(
 
 	Long participationId,
 
-	List<ReplyResponse> replies
+	List<ReplyResponse> replies,
+
+	@Schema(description = "생성일")
+	LocalDateTime createdAt,
+
+	@Schema(description = "수정일")
+	LocalDateTime updatedAt
 ) {
 	public static ReviewedTargetResponse toResponse(ReviewedTarget reviewedTarget, UserResponse user) {
 		return new ReviewedTargetResponse(
 			reviewedTarget.getId(),
 			user,
 			reviewedTarget.getParticipationId(),
-			new ArrayList<>()
+			new ArrayList<>(),
+			reviewedTarget.getCreateAt(),
+			reviewedTarget.getUpdatedAt()
 		);
 	}
 
