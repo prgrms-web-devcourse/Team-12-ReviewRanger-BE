20a584e7ed6c81717117d1a4aa56cdb70e0d9639
sujjangOvO <ssu7038@yu.ac.kr>
2023-11-15 16:14:03 +0900

    feat: 최종 리뷰 응답 DTO swagger 추가

--- src/main/java/com/devcourse/ReviewRanger/finalReviewResult/dto/GetFinalReviewAnswerResponse.java
+++ src/main/java/com/devcourse/ReviewRanger/finalReviewResult/dto/GetFinalReviewAnswerResponse.java
@@ -4,11 +4,23 @@ import java.util.List;
 
 import com.devcourse.ReviewRanger.finalReviewResult.domain.FinalQuestionType;
 
+import io.swagger.v3.oas.annotations.media.Schema;
+
+@Schema(description = "최종 리뷰의 질문과 답변 조회 응답 DTO")
 public record GetFinalReviewAnswerResponse(
+	@Schema(description = "질문 id")
 	Long questionId,
+
+	@Schema(description = "질문 타입")
 	FinalQuestionType finalQuestionType,
+
+	@Schema(description = "질문 제목")
 	String questionTitle,
+
+	@Schema(description = "답변 id 목록")
 	List<Long> answerIdList,
+
+	@Schema(description = "답변 내용 목록")
 	List<Object> answers
 ) {
 }
--- src/main/java/com/devcourse/ReviewRanger/finalReviewResult/dto/GetFinalReviewResultResponse.java
+++ src/main/java/com/devcourse/ReviewRanger/finalReviewResult/dto/GetFinalReviewResultResponse.java
@@ -6,14 +6,32 @@ import java.time.LocalDateTime;
 
 import com.devcourse.ReviewRanger.finalReviewResult.domain.FinalReviewResult;
 
+import io.swagger.v3.oas.annotations.media.Schema;
+
+@Schema(description = "질답을 제외한 최종 리뷰 정보 조회 응답 DTO")
 public record GetFinalReviewResultResponse(
+	@Schema(description = "리뷰 대상 id")
 	Long userId,
+
+	@Schema(description = "리뷰 대상 이름")
 	String userName,
+
+	@Schema(description = "리뷰 id")
 	Long reviewId,
+
+	@Schema(description = "리뷰 제목")
 	String title,
+
+	@Schema(description = "리뷰 설명")
 	String description,
+
+	@Schema(description = "최종 리뷰 상태")
 	Status status,
+
+	@Schema(description = "최종 리뷰 생성 시간")
 	LocalDateTime createdAt,
+
+	@Schema(description = "최종 리뷰 수정 시간")
 	LocalDateTime updatedAt
 ) {
 	public GetFinalReviewResultResponse(FinalReviewResult entity) {
