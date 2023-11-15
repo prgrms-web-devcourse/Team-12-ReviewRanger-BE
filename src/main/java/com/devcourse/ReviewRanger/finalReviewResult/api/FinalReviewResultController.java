8f682dd0a1639a0bf5ca6932bfc5571561efcb5f
sujjangOvO <ssu7038@yu.ac.kr>
2023-11-15 16:14:26 +0900

    feat: 최종 리뷰 API swagger 추가

--- src/main/java/com/devcourse/ReviewRanger/finalReviewResult/api/FinalReviewResultController.java
+++ src/main/java/com/devcourse/ReviewRanger/finalReviewResult/api/FinalReviewResultController.java
@@ -24,6 +24,11 @@ import com.devcourse.ReviewRanger.finalReviewResult.dto.GetFinalReviewAnswerResp
 import com.devcourse.ReviewRanger.finalReviewResult.dto.GetFinalReviewResultResponse;
 import com.devcourse.ReviewRanger.user.domain.UserPrincipal;
 
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
+
+@Tag(name = "final-result", description = "최종 리뷰 결과 API")
 @RestController
 @RequestMapping("/final-results")
 public class FinalReviewResultController {
@@ -34,6 +39,11 @@ public class FinalReviewResultController {
 		this.finalReviewResultService = finalReviewResultService;
 	}
 
+	@Tag(name = "final-result")
+	@Operation(summary = "[토큰] 마이페이지 리뷰 결과 목록 조회", description = "[토큰] 사용자에게 전송 된 리뷰 결과 목록 조회 API", responses = {
+		@ApiResponse(responseCode = "200", description = "전송 된 리뷰 결과 목록 조회 성공"),
+		@ApiResponse(responseCode = "401", description = "토큰을 넣지 않은 경우"),
+	})
 	@GetMapping
 	@ResponseStatus(OK)
 	public RangerResponse<List<FinalReviewResultListResponse>> getAllFinalReviewResults(
@@ -45,6 +55,11 @@ public class FinalReviewResultController {
 		return RangerResponse.ok(allFinalReviewResults);
 	}
 
+	@Tag(name = "final-result")
+	@Operation(summary = "전송 전 최종 리뷰 결과 저장", description = "전송 전 최종 리뷰 결과를 저장 API", responses = {
+		@ApiResponse(responseCode = "201", description = "전송 전 리뷰 결과 저장 성공"),
+		@ApiResponse(responseCode = "404", description = "유효하지 않은 식별 값인 경우"),
+	})
 	@PostMapping
 	@ResponseStatus(CREATED)
 	public RangerResponse<CreateFinalReviewResponse> createFinalReviewResults(
@@ -56,6 +71,11 @@ public class FinalReviewResultController {
 		return RangerResponse.ok(finalReviewResponse);
 	}
 
+	@Tag(name = "final-result")
+	@Operation(summary = "전송 버튼 활성화", description = "전송 전에 해당 리뷰 모든 참여자가 리뷰 결과를 가지는지 검증하는 API", responses = {
+		@ApiResponse(responseCode = "200", description = "전송 버튼 활성화"),
+		@ApiResponse(responseCode = "404", description = "유효하지 않은 식별 값인 경우"),
+	})
 	@GetMapping("/{reviewId}/status")
 	@ResponseStatus(OK)
 	public RangerResponse<Void> checkFinalResultStatus(
@@ -66,6 +86,11 @@ public class FinalReviewResultController {
 		return RangerResponse.ok(checkStatus);
 	}
 
+	@Tag(name = "final-result")
+	@Operation(summary = "리뷰 결과 전송", description = "리뷰 결과를 리뷰 대상에게 전송하는 API", responses = {
+		@ApiResponse(responseCode = "200", description = "해당 리뷰의 리뷰 결과를 참여자에게 전송"),
+		@ApiResponse(responseCode = "404", description = "유효하지 않은 식별 값인 경우"),
+	})
 	@PostMapping("/{reviewId}")
 	@ResponseStatus(OK)
 	public RangerResponse<Void> sendFinalResultToUsers(
@@ -76,17 +101,30 @@ public class FinalReviewResultController {
 		return RangerResponse.noData();
 	}
 
+	@Tag(name = "final-result")
+	@Operation(summary = "[토큰] 리뷰 결과 정보 조회 (Q&A 포함X)", description = "[토큰] 질답 정보 없이 리뷰 결과에 대한 정보를 조회하는 API", responses = {
+		@ApiResponse(responseCode = "200", description = "리뷰 결과 정보 조회 성공"),
+		@ApiResponse(responseCode = "401", description = "토큰을 넣지 않은 경우"),
+		@ApiResponse(responseCode = "404", description = "유효하지 않은 리뷰 결과 id인 경우"),
+		@ApiResponse(responseCode = "409", description = "리뷰 결과 주인이 아닌 경우")
+	})
 	@GetMapping("/{finalReviewId}")
 	@ResponseStatus(OK)
 	public RangerResponse<GetFinalReviewResultResponse> getFinalReviewResultInfo(
+		@AuthenticationPrincipal UserPrincipal user,
 		@PathVariable Long finalReviewId
 	) {
 		GetFinalReviewResultResponse finalReviewResultInfo
-			= finalReviewResultService.getFinalReviewResultInfo(finalReviewId);
+			= finalReviewResultService.getFinalReviewResultInfo(user.getId(), finalReviewId);
 
 		return RangerResponse.ok(finalReviewResultInfo);
 	}
 
+	@Tag(name = "final-result")
+	@Operation(summary = "리뷰 결과 Q&A 정보 조회", description = "리뷰 결과의 질문과 답변에 대한 목록을 조회하는 API", responses = {
+		@ApiResponse(responseCode = "200", description = "리뷰 결과의 질답 목록 정보 조회 성공"),
+		@ApiResponse(responseCode = "404", description = "유효하지 않은 식별 값인 경우"),
+	})
 	@GetMapping("/{finalReviewId}/qna")
 	@ResponseStatus(OK)
 	public RangerResponse<List<GetFinalReviewAnswerResponse>> getFinalReviewAnswers(
