d5a5d7b64d30c210c1d2cbbd8e305957b6f92424
beombu <beombu13@gmail.com>
2023-11-17 14:42:05 +0900

    [REV-230] rafactor: QueryDSL 객체 수정

--- src/main/java/com/devcourse/ReviewRanger/ReplyTarget/repository/ReplyTargetCustomRepositoryImpl.java
+++ src/main/java/com/devcourse/ReviewRanger/ReplyTarget/repository/ReplyTargetCustomRepositoryImpl.java
@@ -1,10 +1,11 @@
 package com.devcourse.ReviewRanger.ReplyTarget.repository;
 
+import static com.devcourse.ReviewRanger.ReplyTarget.domain.QReplyTarget.*;
+
 import java.util.List;
 
 import org.springframework.util.StringUtils;
 
-import com.devcourse.ReviewRanger.ReplyTarget.domain.QReplyTarget;
 import com.devcourse.ReviewRanger.ReplyTarget.domain.ReplyTarget;
 import com.querydsl.core.BooleanBuilder;
 import com.querydsl.jpa.impl.JPAQueryFactory;
@@ -12,8 +13,6 @@ import com.querydsl.jpa.impl.JPAQueryFactory;
 public class ReplyTargetCustomRepositoryImpl implements ReplyTargetCustomRepository {
 
 	private final JPAQueryFactory jpaQueryFactory;
-
-	QReplyTarget qReplyTarget = QReplyTarget.replyTarget;
 
 	public ReplyTargetCustomRepositoryImpl(JPAQueryFactory jpaQueryFactory) {
 		this.jpaQueryFactory = jpaQueryFactory;
@@ -23,16 +22,16 @@ public class ReplyTargetCustomRepositoryImpl implements ReplyTargetCustomReposit
 	public List<ReplyTarget> findAllByParticipationIdToDynamic(Long participationId, String searchName) {
 		BooleanBuilder builder = new BooleanBuilder();
 		if (participationId != null) {
-			builder.and(qReplyTarget.participationId.eq(participationId));
+			builder.and(replyTarget.participationId.eq(participationId));
 		}
 
 		if (StringUtils.hasText(searchName)) {
-			builder.and(qReplyTarget.receiver.name.contains(searchName));
+			builder.and(replyTarget.receiver.name.contains(searchName));
 		}
 
-		List<ReplyTarget> replyTargets = jpaQueryFactory.selectFrom(qReplyTarget)
+		List<ReplyTarget> replyTargets = jpaQueryFactory.selectFrom(replyTarget)
 			.where(builder)
-			.orderBy(qReplyTarget.receiver.name.asc())
+			.orderBy(replyTarget.receiver.name.asc())
 			.fetch();
 
 		return replyTargets;
