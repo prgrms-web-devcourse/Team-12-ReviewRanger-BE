3409beaa8f66c01845dbe5e7509cd001b5462e5a
beombu <beombu13@gmail.com>
2023-11-18 10:26:56 +0900

    [REV-248] test: 답변 참여 성공 및 조회 실패 테스트 추가

--- src/test/java/com/devcourse/ReviewRanger/participation/application/ParticipationServiceTest.java
+++ src/test/java/com/devcourse/ReviewRanger/participation/application/ParticipationServiceTest.java
@@ -1,5 +1,13 @@
 package com.devcourse.ReviewRanger.participation.application;
 
+import static com.devcourse.ReviewRanger.common.exception.ErrorCode.*;
+import static org.mockito.BDDMockito.*;
+
+import java.util.Optional;
+
+import org.assertj.core.api.Assertions;
+import org.assertj.core.util.Lists;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
@@ -8,7 +16,11 @@ import org.mockito.junit.jupiter.MockitoExtension;
 
 import com.devcourse.ReviewRanger.ReplyTarget.application.ReplyTargetService;
 import com.devcourse.ReviewRanger.ReplyTarget.repository.ReplyTargetRepository;
+import com.devcourse.ReviewRanger.common.exception.RangerException;
+import com.devcourse.ReviewRanger.participation.domain.Participation;
+import com.devcourse.ReviewRanger.participation.dto.request.SubmitParticipationRequest;
 import com.devcourse.ReviewRanger.participation.repository.ParticipationRepository;
+import com.devcourse.ReviewRanger.user.domain.User;
 import com.devcourse.ReviewRanger.user.repository.UserRepository;
 
 @ExtendWith(MockitoExtension.class)
@@ -29,8 +41,40 @@ class ParticipationServiceTest {
 	@Mock
 	private ReplyTargetRepository replyTargetRepository;
 
-	@Test
-	void 답변_저장_성공() {
+	private User user1;
+	private User user2;
 
+	@BeforeEach
+	public void setup() {
+		user1 = new User("수연", "tttttaa@naver.com", "asdf12345");
+		user2 = new User("범철", "tttttbb@naver.com", "asdf12345");
+	}
+
+	@Test
+	void 답변_참여_성공() {
+		//given
+		SubmitParticipationRequest submitParticipationRequest = new SubmitParticipationRequest(1L,
+			Lists.emptyList());
+		Participation participation = new Participation(user1);
+
+		given(participationRepository.findById(submitParticipationRequest.participationId())).willReturn(
+			Optional.of(participation));
+
+		//when
+		participationService.submitReplies(submitParticipationRequest);
+
+		//when
+		Assertions.assertThat(participation.getIsAnswered()).isTrue();
+	}
+
+	@Test
+	void 참여_조회_실패() {
+		//given
+		Long participationId = 9L;
+
+		//when & then
+		Assertions.assertThatThrownBy(() -> participationService.getByIdOrThrow(participationId))
+			.isInstanceOf(RangerException.class)
+			.hasMessage(NOT_FOUND_PARTICIPATION.getMessage());
 	}
 }
