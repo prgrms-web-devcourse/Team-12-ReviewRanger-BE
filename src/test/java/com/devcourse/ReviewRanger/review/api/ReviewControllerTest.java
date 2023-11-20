1026073b4095dcb6e7b157b78b3ea600e3a46ce3
beombu <beombu13@gmail.com>
2023-11-20 14:47:06 +0900

    [REV-246] merge: conflict 해결

--- src/test/java/com/devcourse/ReviewRanger/review/api/ReviewControllerTest.java
+++ src/test/java/com/devcourse/ReviewRanger/review/api/ReviewControllerTest.java
@@ -1,45 +1,42 @@
 package com.devcourse.ReviewRanger.review.api;
 
+import static com.devcourse.ReviewRanger.user.service.TestPrincipalDetailsService.*;
 import static java.time.LocalDateTime.*;
 import static org.mockito.BDDMockito.*;
-import static com.devcourse.ReviewRanger.user.service.TestPrincipalDetailsService.*;
-import static org.mockito.Mockito.*;
 import static org.springframework.security.test.web.servlet.request.SecurityMockMvcRequestPostProcessors.*;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.*;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
+import java.time.LocalDateTime;
 import java.util.ArrayList;
 import java.util.List;
 
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
-
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.context.annotation.Import;
 import org.springframework.data.jpa.mapping.JpaMetamodelMappingContext;
 import org.springframework.http.MediaType;
-
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 
-import com.devcourse.ReviewRanger.user.service.TestPrincipalDetailsService;
 import com.devcourse.ReviewRanger.common.config.SecurityConfig;
 import com.devcourse.ReviewRanger.common.jwt.JwtTokenProvider;
 import com.devcourse.ReviewRanger.participation.application.ParticipationService;
 import com.devcourse.ReviewRanger.participation.domain.ReviewStatus;
 import com.devcourse.ReviewRanger.participation.dto.response.ParticipationResponse;
-
+import com.devcourse.ReviewRanger.participation.dto.response.ReceiverResponse;
 import com.devcourse.ReviewRanger.question.dto.request.CreateQuestionRequest;
 import com.devcourse.ReviewRanger.review.application.ReviewService;
 import com.devcourse.ReviewRanger.review.domain.ReviewType;
 import com.devcourse.ReviewRanger.review.dto.request.CreateReviewRequest;
 import com.devcourse.ReviewRanger.review.dto.response.ReviewResponse;
 import com.devcourse.ReviewRanger.user.dto.UserResponse;
-
+import com.devcourse.ReviewRanger.user.service.TestPrincipalDetailsService;
 import com.fasterxml.jackson.databind.ObjectMapper;
 
 @WebMvcTest(ReviewController.class)
@@ -61,12 +58,12 @@ class ReviewControllerTest {
 
 	@MockBean
 	private ParticipationService participationService;
-  
-  private TestPrincipalDetailsService testUserDetailsService;
+
+	private TestPrincipalDetailsService testUserDetailsService;
 	private UserDetails userDetails;
 
 	@BeforeEach
-	public void setUp(){
+	public void setUp() {
 		testUserDetailsService = new TestPrincipalDetailsService();
 		userDetails = testUserDetailsService.loadUserByUsername(USER_EMAIL);
 	}
@@ -137,8 +134,8 @@ class ReviewControllerTest {
 			.andExpect(status().isOk())
 			.andDo(print());
 	}
-  
-  @Test
+
+	@Test
 	void 리뷰_생성_성공() throws Exception {
 		// given
 		String title = "테스트 리뷰";
@@ -155,7 +152,7 @@ class ReviewControllerTest {
 			responserIds
 		);
 
-		when(reviewService.createReview(null,createReviewRequest)).thenReturn(true);
+		when(reviewService.createReview(null, createReviewRequest)).thenReturn(true);
 
 		// when
 		// then
@@ -167,6 +164,6 @@ class ReviewControllerTest {
 			.andExpect(jsonPath("$.success").value(true))
 			.andDo(print());
 
-		verify(reviewService, times(1)).createReview(null,createReviewRequest);
+		verify(reviewService, times(1)).createReview(null, createReviewRequest);
 	}
 }
