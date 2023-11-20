5575280de2cff18f1f907f333da7ef9bf827ee6d
juwoong kim <wwong7247@gmail.com>
2023-11-20 16:32:40 +0900

    [REV-216] 질문 description 필드 추가에 따른 테스트 코드 적용

--- src/test/java/com/devcourse/ReviewRanger/question/application/QuestionServiceTest.java
+++ src/test/java/com/devcourse/ReviewRanger/question/application/QuestionServiceTest.java
@@ -24,12 +24,14 @@ class QuestionServiceTest {
 	void 질문_생성_성공() {
 		// given
 		String title = "질문 내용입니다.";
+		String description = "질문 설명입니다.";
 		QuestionType type = QuestionType.SUBJECTIVE;
 		Boolean isRequired = true;
 		List<CreateQuestionOptionRequest> questionOptions = List.of();
 
 		CreateQuestionRequest createQuestionRequest = new CreateQuestionRequest(
 			title,
+			description,
 			type,
 			isRequired,
 			questionOptions
@@ -48,12 +50,14 @@ class QuestionServiceTest {
 	void 질문제목_빈값에_따른_질문_생성_실패() {
 		// given
 		String title = "";
+		String description = "질문 설명입니다.";
 		QuestionType type = QuestionType.SUBJECTIVE;
 		Boolean isRequired = true;
 		List<CreateQuestionOptionRequest> questionOptions = List.of();
 
 		CreateQuestionRequest createQuestionRequest = new CreateQuestionRequest(
 			title,
+			description,
 			type,
 			isRequired,
 			questionOptions
