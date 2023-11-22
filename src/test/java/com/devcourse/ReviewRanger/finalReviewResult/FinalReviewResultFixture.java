9f8c5e74970cbe484f2418f40e52d24fc5f47d18
sujjangOvO <ssu7038@yu.ac.kr>
2023-11-22 23:57:10 +0900

    feat: 최종 리뷰 결과 도메인 Fixture 추가

---
+++ src/test/java/com/devcourse/ReviewRanger/finalReviewResult/FinalReviewResultFixture.java
@@ -0,0 +1,58 @@
+package com.devcourse.ReviewRanger.finalReviewResult;
+
+import static com.devcourse.ReviewRanger.FinalQuestionFixture.*;
+import static com.devcourse.ReviewRanger.finalReviewResult.domain.FinalReviewResult.*;
+import static com.devcourse.ReviewRanger.finalReviewResult.domain.FinalReviewResult.Status.*;
+
+import java.util.List;
+
+import com.devcourse.ReviewRanger.finalReviewResult.domain.FinalQuestion;
+import com.devcourse.ReviewRanger.finalReviewResult.domain.FinalReviewResult;
+
+public enum FinalReviewResultFixture {
+
+	BASIC_FIXTURE(1L,
+		"장수연",
+		1L,
+		"1차 피어리뷰 입니다.",
+		"마감일은 2025-25-25입니다.",
+		NOT_SENT,
+		List.of(
+			ONCE_QUESTION.toEntity(),
+			TWICE_QUESTION.toEntity(),
+			TWICE_QUESTION.toEntity(),
+			FOUR_QUESTION.toEntity(),
+			FIVE_QUESTION.toEntity(),
+			SIX_QUESTION.toEntity()
+		)
+	);
+
+	private final Long userId;
+
+	private final String userName;
+
+	private final Long reviewId;
+
+	private final String title;
+
+	private final String description;
+
+	private final Status status;
+
+	private final List<FinalQuestion> questions;
+
+	FinalReviewResultFixture(Long userId, String userName, Long reviewId, String title, String description,
+		Status status, List<FinalQuestion> questions) {
+		this.userId = userId;
+		this.userName = userName;
+		this.reviewId = reviewId;
+		this.title = title;
+		this.description = description;
+		this.status = status;
+		this.questions = questions;
+	}
+
+	public FinalReviewResult toEntity() {
+		return new FinalReviewResult(userId, userName, reviewId, title, description, status, questions);
+	}
+}
