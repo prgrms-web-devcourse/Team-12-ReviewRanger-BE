33e2b6b18cbcbd40653965d4e5c6823fbc6c9bc9
sujjangOvO <ssu7038@yu.ac.kr>
2023-11-22 16:20:20 +0900

    [REV-311] modify: 유저 도메인 정규식 수정

--- src/main/java/com/devcourse/ReviewRanger/common/regex/UserRegex.java
+++ src/main/java/com/devcourse/ReviewRanger/common/regex/UserRegex.java
@@ -2,6 +2,6 @@ package com.devcourse.ReviewRanger.common.regex;
 
 public class UserRegex {
 	public static final String NAME_REGEXP = "^[가-힣a-zA-Z\\d\\[\\]<>-]{2,20}";
-	public static final String EMAIL_REGEXP = "[a-z0-9]+@[a-z]+\\\\.[a-z]{2,8}$";
+	public static final String EMAIL_REGEXP = "[a-z0-9]+@[a-z]+\\.[a-z]{2,8}$";
 	public static final String PASSWORD_REGEXP = "^(?=.*[a-zA-Z])(?=.*[0-9]).{8,25}$";
 }
