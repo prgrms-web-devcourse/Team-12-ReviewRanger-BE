0a5b500eba88c195349aa91672be077407ef9d02
sujjangOvO <ssu7038@yu.ac.kr>
2023-11-23 16:15:09 +0900

    test: 유저 도메인 로그인 단위 테스트 추가

---
+++ src/test/java/com/devcourse/ReviewRanger/auth/application/AuthServiceTest.java
@@ -0,0 +1,77 @@
+package com.devcourse.ReviewRanger.auth.application;
+
+import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.Mockito.*;
+
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.security.crypto.password.PasswordEncoder;
+
+import com.devcourse.ReviewRanger.auth.dto.JoinRequest;
+import com.devcourse.ReviewRanger.common.exception.RangerException;
+import com.devcourse.ReviewRanger.user.domain.User;
+import com.devcourse.ReviewRanger.user.repository.UserRepository;
+
+@ExtendWith(MockitoExtension.class)
+public class AuthServiceTest {
+
+	@InjectMocks
+	private AuthService authService;
+
+	@Mock
+	private UserRepository userRepository;
+
+	@Mock
+	private PasswordEncoder passwordEncoder;
+
+	@Test
+	void 회원가입_성공_테스트() {
+		// given
+		JoinRequest mockRequest = mock(JoinRequest.class);
+		when(mockRequest.password()).thenReturn("mockPassword");
+		when(mockRequest.toEntity(anyString())).thenReturn(mock(User.class));
+		when(passwordEncoder.encode(anyString())).thenReturn("encryptedPassword");
+
+		User savedUser = mock(User.class);
+
+		when(userRepository.save(any(User.class))).thenReturn(savedUser);
+		when(savedUser.getId()).thenReturn(1L);
+
+		// when
+		Boolean result = authService.join(mockRequest);
+
+		// then
+		verify(passwordEncoder, times(1)).encode("mockPassword");
+		verify(userRepository, times(1)).save(any(User.class));
+		assertTrue(result);
+	}
+
+	@Test
+	void 회원가입_실패_이메일중복_테스트() {
+		// given
+		JoinRequest joinRequest = new JoinRequest("장수연", "dev12341@devcource.com", "abc12341234");
+		when(passwordEncoder.encode(anyString())).thenReturn("encryptedPassword");
+		when(userRepository.existsByEmail(anyString())).thenReturn(true);
+
+		// when & then
+		assertThrows(RangerException.class,
+			() -> authService.join(joinRequest)
+		);
+	}
+
+	@Test
+	void 회원가입_실패_이름중복_테스트() {
+		// given
+		JoinRequest joinRequest = new JoinRequest("장수연", "dev12341@devcource.com", "abc12341234");
+		when(passwordEncoder.encode(any())).thenReturn("encryptedPassword");
+		when(userRepository.existsByName(anyString())).thenReturn(true);
+
+		// when & then
+		assertThrows(RangerException.class,
+			() -> authService.join(joinRequest)
+		);
+	}
+}
