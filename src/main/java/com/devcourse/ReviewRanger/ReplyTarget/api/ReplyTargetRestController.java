972d1aa7c340ccfec7eb6821baf586f88c3ae77a
beombu <beombu13@gmail.com>
2023-11-25 18:06:55 +0900

    [REV-327] fix: cicd 테스트용 컨트롤러 삭제

--- src/main/java/com/devcourse/ReviewRanger/ReplyTarget/api/ReplyTargetRestController.java
+++ src/main/java/com/devcourse/ReviewRanger/ReplyTarget/api/ReplyTargetRestController.java
@@ -52,15 +52,4 @@ public class ReplyTargetRestController {
 
 		return RangerResponse.ok(responses);
 	}
-
-	@Tag(name = "reply-target")
-	@Operation(summary = "테스트1", description = "테스트 API", responses = {
-		@ApiResponse(responseCode = "200", description = "테스트 성공"),
-	})
-	@GetMapping("/reviews/{reviewId}/test/lastTest")
-	public RangerResponse<Void> getRepliesByReceiver(@PathVariable Long reviewId) {
-		System.out.println(reviewId);
-
-		return RangerResponse.noData();
-	}
 }
