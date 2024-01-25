a7d098a94675bf31c5834ad30afbbd01f5d30183
beombu <beombu13@gmail.com>
2024-01-25 18:22:54 +0900

    [REV-407] refactor: UserFixture imagePath 추가

--- src/test/java/com/devcourse/ReviewRanger/user/UserFixture.java
+++ src/test/java/com/devcourse/ReviewRanger/user/UserFixture.java
@@ -1,25 +1,26 @@
 package com.devcourse.ReviewRanger.user;
 
-import com.devcourse.ReviewRanger.user.domain.User;
 import com.devcourse.ReviewRanger.auth.dto.JoinRequest;
+import com.devcourse.ReviewRanger.user.domain.User;
 import com.devcourse.ReviewRanger.user.dto.GetUserResponse;
-import com.devcourse.ReviewRanger.user.dto.UserResponse;
 
 public enum UserFixture {
 
-	SUYEON_FIXTURE("장수연", "dev12341@devcource.com", "abc12341234"),
-	BEOMCHUL_FIXTURE("신범철", "dev12342@devcource.com", "abc12341234"),
-	JUWOONG_FIXTURE("김주웅", "dev12343@devcource.com", "abc12341234"),
-	SPENCER_FIXTURE("스펜서", "dev12344@devcource.com", "abc12341234");
+	SUYEON_FIXTURE("장수연", "dev12341@devcource.com", "abc12341234", "이미지경로1"),
+	BEOMCHUL_FIXTURE("신범철", "dev12342@devcource.com", "abc12341234", "이미지경로1"),
+	JUWOONG_FIXTURE("김주웅", "dev12343@devcource.com", "abc12341234", "이미지경로1"),
+	SPENCER_FIXTURE("스펜서", "dev12344@devcource.com", "abc12341234", "이미지경로1");
 
 	private final String name;
 	private final String email;
 	private final String password;
+	private final String path;
 
-	UserFixture(String name, String email, String password) {
+	UserFixture(String name, String email, String password, String path) {
 		this.name = name;
 		this.email = email;
 		this.password = password;
+		this.path = path;
 	}
 
 	public User toEntity() {
@@ -30,8 +31,8 @@ public enum UserFixture {
 		return new JoinRequest(name, email, password);
 	}
 
-	public GetUserResponse toGetUserResponse(){
-		Long tempUserId =1L;
-		return new GetUserResponse(tempUserId,name);
+	public GetUserResponse toGetUserResponse() {
+		Long tempUserId = 1L;
+		return new GetUserResponse(tempUserId, name, path);
 	}
 }
